3a994ac5bf4abeea1beb38b23023f09b2cc4bf53
Miguel Amorós <miguel14amoros@gmail.com>
2019-11-25 08:36:22 +0100

    correcions from scratch

---
+++ hostpam19:scratch/install.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+useradd local1
+useradd local2
+useradd local3
+echo "local1" | passwd --stdin local1
+echo "local2" | passwd --stdin local2
+echo "local3" | passwd --stdin local3
+cp /opt/docker/login.defs /etc/login.defs
+cp /opt/docker/nslcd.conf /etc/nslcd.conf
+cp /opt/docker/nslcd.conf /etc/nscd.conf
+cp /opt/docker/nsswitch.conf /etc/nsswitch.conf
+authconfig --enableshadow --enablelocauthorize --enableldap --enableldapauth --enablemkhomedir --ldapserver='ldapserver' --ldapbase='dc=edt,dc=org' --updateall
+#bash /opt/docker/auth.sh
