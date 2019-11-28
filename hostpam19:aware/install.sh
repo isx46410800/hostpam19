94b0717c706163a00aa3f17d5caaa54eda796b4f
Miguel Amorós <miguel14amoros@gmail.com>
2019-11-28 12:02:32 +0100

    hostpam19:aware

--- hostpam19:aware/install.sh
+++ hostpam19:aware/install.sh
@@ -7,3 +7,4 @@ cp /opt/docker/login.defs /etc/login.defs
 cp /opt/docker/nslcd.conf /etc/nslcd.conf
 cp /opt/docker/nslcd.conf /etc/nscd.conf
 cp /opt/docker/nsswitch.conf /etc/nsswitch.conf
+cp /opt/docker/pam_python.so /usr/lib64/security/.
