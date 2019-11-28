a4ca83af2692df56b5a0ae0edfc79445c8a91882
Miguel Amorós <miguel14amoros@gmail.com>
2019-11-28 12:30:33 +0100

    hostpam19:aware

--- hostpam19:aware/pam_mates.py
+++ hostpam19:aware/pam_mates.py
@@ -5,25 +5,26 @@
 
 # Validar l’usuari realitzant una pregunta de matemàtiques
 import pam
-def ​ pam_sm_authenticate ( ​ pamh, flags, argv):
-	print "Quant fan 3*2?"
-	resposta=raw_input()
+def pam_sm_authenticate(pamh, flags, argv):
+	print ("Quant fan 3*2?")
+	resposta=input()
 	if int(resposta) == 6:
 		return pamh.PAM_SUCCESS
 	else:
 		return pamh.PAM_AUTHTOK_ERR
 		
-def ​ pam_sm_setcred ​ (pamh, flags, argv):
+
+def pam_sm_setcred(pamh, flags, argv):
 	return pamh.PAM_SUCCESS
 	
-def ​ pam_sm_acct_mgmt ( ​ pamh, flags, argv):
+def pam_sm_acct_mgmt(pamh, flags, argv):
 	return pamh.PAM_SUCCESS
 	
-def ​ pam_sm_open_session ( ​ pamh, flags, argv):
+def pam_sm_open_session(pamh, flags, argv):
 	return pamh.PAM_SUCCESS
 	
-def ​ pam_sm_close_session ​ (pamh, flags, argv):
+def pam_sm_close_session(pamh, flags, argv):
 	return pamh.PAM_SUCCESS
 	
-def ​ pam_sm_chauthtok ​ (pamh, flags, argv):
+def pam_sm_chauthtok(pamh, flags, argv):
 	return pamh.PAM_SUCCESS
